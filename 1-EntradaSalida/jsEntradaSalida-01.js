7a8fafad24c7e1711534d172e6c7ed6a74dfe4d8
arozas <alejdro.rozas@gmail.com>
2022-01-31 15:13:07 -0300

    guardé el ejericio resuelto

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,6 +2,8 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
+
+// resolví el ejercicio mientras veo la clase grabada
 {
 	alert("Esto funciona de maravilla");
 }
