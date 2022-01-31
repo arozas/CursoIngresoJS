7b2e184a24f20be93d69c2c42d8433f2228a799c
arozas <alejdro.rozas@gmail.com>
2022-01-31 19:56:53 -0300

    Ejercicio 2 resuelto

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,19 @@
 /*
+Alejandro Alberto Martín Rozas
+e/s 02
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	//alert("ok");
+	/*let person = prompt("Please enter your name", "Harry Potter"); 
+	primera interacción con el código, 
+	w3school son ejemplos de programacion no siempre es codigo bien programado*/
+	let nombreIngresado = prompt("Por favor ingrese su nombre", "Fulano");
+
+	//alert("texto literal")
+	//alert(person) ejemplo de salida de la variable anterior
+	alert("su nombre es "+ nombreIngresado)
+	//alert(nombreIngresado)
 }
 
