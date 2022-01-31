cf6a285253b02d1a5f6a2d9aeba56a6dbeb92aef
arozas <alejdro.rozas@gmail.com>
2022-01-31 20:06:49 -0300

    Resuelto y corregido el ejercicio 2 con notas nuevas

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -11,9 +11,9 @@ function mostrar()
 	w3school son ejemplos de programacion no siempre es codigo bien programado*/
 	let nombreIngresado = prompt("Por favor ingrese su nombre", "Fulano");
 
-	//alert("texto literal")
+	//alert("texto literal") todo lo que ponemos entre comillas sale literal en el mensaje de alerta
 	//alert(person) ejemplo de salida de la variable anterior
-	alert("su nombre es "+ nombreIngresado)
+	alert("su nombre es "+ nombreIngresado); // se agrega espacio entre las comillas para que al concatenar no salga todo junto.
 	//alert(nombreIngresado)
 }
 
