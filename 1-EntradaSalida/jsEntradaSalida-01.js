1af2e4ed4890ed4c07739be6bb438da08f3b260e
arozas <alejdro.rozas@gmail.com>
2022-01-31 19:27:42 -0300

    resuelto ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,10 +1,13 @@
 /*
+Alejandro Alberto Martín Rozas
+e/s 01
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 
 // resolví el ejercicio mientras veo la clase grabada
 {
+	//Alert("Hola Mundo") Es case sensitive, y Alert en mayuscula no funciona.
 	alert("Esto funciona de maravilla");
 }
 
